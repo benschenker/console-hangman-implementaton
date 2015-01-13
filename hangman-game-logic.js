4355256abfe077aaca06cb194052aa37cbcc3bbf
Ben Schenker <binsch@gmail.com>
2015-01-13 00:12:27 +0000

    Fix indentation and add missing semicolons

--- hangman-game-logic.js
+++ hangman-game-logic.js
@@ -1,79 +1,78 @@
 (function() {
-                'use strict'
-                /////////////////////////////////////////////////
-                // Variable declaration
-                /////////////////////////////////////////////////
-                var phrase, setPhrase, guess, charactersGuessed, displayCurrent
-                    ;
+    'use strict';
+    /////////////////////////////////////////////////
+    // Variable declaration
+    /////////////////////////////////////////////////
+    var phrase, setPhrase, guess, charactersGuessed, displayCurrent;
 
-                /////////////////////////////////////////////////
-                // Variable initialization
-                /////////////////////////////////////////////////
-                setPhrase = function(text) {
-                    if(phrase){
-                        return "Oh no, you already set the phrase!";
-                    } else {
-                        phrase = text.toUpperCase();
-                        return "Congrats, you set the phrase!";
-                    }
-                };
+    /////////////////////////////////////////////////
+    // Variable initialization
+    /////////////////////////////////////////////////
+    setPhrase = function(text) {
+        if(phrase){
+            return "Oh no, you already set the phrase!";
+        } else {
+            phrase = text.toUpperCase();
+            return "Congrats, you set the phrase!";
+        }
+    };
 
-                charactersGuessed = [];
-                guess = function(character) {
-                    if(character) {
-                        charactersGuessed.push(character.toUpperCase());
-                    }
-                    return displayCurrent();
+    charactersGuessed = [];
+    guess = function(character) {
+        if(character) {
+            charactersGuessed.push(character.toUpperCase());
+        }
+        return displayCurrent();
+    };
+
+    displayCurrent = function() {
+        var displayedPhrase, unknownLettersRemaining, numIncorrect;
+        /////////////////////////////////////////////////
+        unknownLettersRemaining = 0;
+        numIncorrect = 0;
+
+        displayedPhrase = window.hangman.forEachLetter(phrase, function(character) {
+            if(character === " ") {
+                return character;
+            } else if (window.hangman.contains(charactersGuessed, character)) {
+                return character;
+            } else {
+                unknownLettersRemaining = unknownLettersRemaining + 1;
+                return "-";
+            }
+        });
+        window.hangman.forEachLetter(
+            charactersGuessed.join(''), function(character) {
+                if (phrase.indexOf(character)===-1) {
+                    numIncorrect++;
                 }
+        });
+        /////////////////////////////////////////////////
+        console.log("Currently:", displayedPhrase);
+        console.log("So far you've guessed ", charactersGuessed);
 
-                displayCurrent = function() {
-                    var displayedPhrase, unknownLettersRemaining, numIncorrect;
-                    /////////////////////////////////////////////////
-                    unknownLettersRemaining = 0;
-                    numIncorrect = 0;
+        if(unknownLettersRemaining === 0) {
+            console.log("You win!");
+            window.hangman.cornifyTimes(4);
+        }
+        return {
+            secretState: displayedPhrase,
+            numIncorrect: numIncorrect
+        };
+    };
 
-                    displayedPhrase = window.hangman.forEachLetter(phrase, function(character) {
-                        if(character === " ") {
-                            return character;
-                        } else if (window.hangman.contains(charactersGuessed, character)) {
-                            return character;
-                        } else {
-                            unknownLettersRemaining = unknownLettersRemaining + 1;
-                            return "-";
-                        }
-                    });
-                    window.hangman.forEachLetter(
-                        charactersGuessed.join(''), function(character) {
-                            if (phrase.indexOf(character)===-1) {
-                                numIncorrect++;
-                            }
-                    });
-                    /////////////////////////////////////////////////
-                    console.log("Currently:", displayedPhrase);
-                    console.log("So far you've guessed ", charactersGuessed);
+    /////////////////////////////////////////////////
+    // Do Stuff
+    /////////////////////////////////////////////////
+    console.log("Welcome to hangman! There is a secret phrase");
+    console.log("Commands you can use: guess('a')");
 
-                    if(unknownLettersRemaining === 0) {
-                        console.log("You win!");
-                        window.hangman.cornifyTimes(4);
-                    }
-                    return {
-                        secretState: displayedPhrase,
-                        numIncorrect: numIncorrect
-                    }
-                }
+    // displayCurrent();
 
-                /////////////////////////////////////////////////
-                // Do Stuff
-                /////////////////////////////////////////////////
-                console.log("Welcome to hangman! There is a secret phrase");
-                console.log("Commands you can use: guess('a')");
-
-                // displayCurrent();
-
-                /////////////////////////////////////////////////
-                // Export variables
-                /////////////////////////////////////////////////
-                window.guess = guess;
-                window.displayCurrent = displayCurrent;
-                window.setPhrase = setPhrase;
-            })();
+    /////////////////////////////////////////////////
+    // Export variables
+    /////////////////////////////////////////////////
+    window.guess = guess;
+    window.displayCurrent = displayCurrent;
+    window.setPhrase = setPhrase;
+})();
